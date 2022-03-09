ff848af1ab410c776c8915b6b58bc556bbe89032
jhanvi310 <74711282+jhanvi310@users.noreply.github.com>
2022-03-09 14:33:40 +0530

    Update App.java

--- printmessage/src/main/java/printmessage/App.java
+++ printmessage/src/main/java/printmessage/App.java
@@ -1,7 +1,15 @@
 package printmessage;
+import org.springframework.boot.SpringApplication;
 
+import reactor.core.publisher.Flux;
 public class App {
 	public static void main(String [] args){
 		System.out.println("Hello World!!"); 
+		Flux<String> dataStream = Flux.just("Hello World");
+		SpringApplication.run(App.class, args);
+		dataStream.subscribe(prog ->
+		{
+		 System.out.println(prog);
+		});
 	}
 }
